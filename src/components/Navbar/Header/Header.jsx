fff4e619164cf2dfb1d12ef80bbccd2852cd838e
Abdulloh <abdurahmonovabdulloh379@gmail.com>
2024-09-05 18:53:23 +0500

    first project

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose food dolor sit amet consectetur adipisicing elit. Incidunt tempore earum, laboriosam officiis, doloremque magnam maiores, expedita nemo voluptatum odio culpa molestiae dicta sed aliquid nulla at accusantium. Odio, sed?</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
