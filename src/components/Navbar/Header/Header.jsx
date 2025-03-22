e656d9bc0978de52ccdf19c78caeaa58de7394aa
cxAbdulloh <168731472+cxAbdulloh@users.noreply.github.com>
2025-03-22 15:58:14 +0500

    change image

--- src/components/Navbar/Header/Header.jsx
+++ src/components/Navbar/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Header.css'
-
+import { assets } from '../../../assets/assets'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{ backgroundImage: `url(${assets.header_img})` }}>
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose food dolor sit amet consectetur adipisicing elit. Incidunt tempore earum, laboriosam officiis, doloremque magnam maiores, expedita nemo voluptatum odio culpa molestiae dicta sed aliquid nulla at accusantium. Odio, sed?</p>
